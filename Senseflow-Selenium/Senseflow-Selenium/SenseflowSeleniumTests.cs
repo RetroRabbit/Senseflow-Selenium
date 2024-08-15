8a8330666c6e7c4474501cd262bc3b48ba555f34
SP <sdebeer@retrorabbit.co.za>
2024-08-15 15:12:46 +0200

    Fixed Double Chrome Windows + Successfully Implemented Explicit Waits
    Reverted back to a simpler structure to get everything working

--- Senseflow-Selenium/Senseflow-Selenium/SenseflowSeleniumTests.cs
+++ Senseflow-Selenium/Senseflow-Selenium/SenseflowSeleniumTests.cs
@@ -1,46 +1,58 @@
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
 using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium.Chrome;
 using System.Threading;
 
 namespace Senseflow_Selenium
 {
     [Collection("SFTest")]
-    public class SenseflowSeleniumTests : IClassFixture<SenseflowBrowserFixtures>
+    public class SenseflowSeleniumTests
     {
-        private readonly SenseflowBrowserFixtures _fixture;
+        WebDriver chromeDriver;
+        WebDriverWait webDriverWait;
         private IConfiguration Configuration { get; }
 
 
-        public SenseflowSeleniumTests(SenseflowBrowserFixtures fixture)
+        public SenseflowSeleniumTests()
         {
-            _fixture = fixture;
+            chromeDriver = new ChromeDriver();
+            webDriverWait = new WebDriverWait(chromeDriver, TimeSpan.FromSeconds(10));
             var builder = new ConfigurationBuilder().AddUserSecrets<SenseflowSeleniumTests>();
             Configuration = builder.Build();
         }
 
         [Fact]
         public void SenseflowLoginTest()
-        {
-            _fixture.ChromeWebDriver.Navigate().GoToUrl("https://app.senseflow.ai/time");
+        {           
+            chromeDriver.Navigate().GoToUrl("https://app.senseflow.ai/time");              
+            webDriverWait.Until(d => d.Url.Contains("auth.senseflow.ai"));
 
-            Thread.Sleep(1000);
-            _fixture.ChromeWebDriver.FindElement(By.Name("organizationName")).SendKeys("RetroRabbit");
-            _fixture.ChromeWebDriver.FindElement(By.Name("action")).Click();
+            IWebElement orgTextBox = chromeDriver.FindElement(By.Name("organizationName"));
+            webDriverWait.Until(d => orgTextBox.Displayed);
+            orgTextBox.SendKeys("RetroRabbit");
+            chromeDriver.FindElement(By.Name("action")).Click();
 
-            Thread.Sleep(2000);
-            IWebElement searchBox = _fixture.ChromeWebDriver.FindElement(By.Id("identifierId"));
+            webDriverWait.Until(d => d.Url.Contains("https://accounts.google.com/v3/signin/identifier"));
+            IWebElement searchBox = chromeDriver.FindElement(By.Id("identifierId"));
+            webDriverWait.Until(d => searchBox.Displayed);
             searchBox.SendKeys(Configuration["username"]);
-            searchBox.SendKeys(Keys.Enter);
+            searchBox.SendKeys(Keys.Enter);        
 
-            Thread.Sleep(3000);
-            IWebElement passwordBox = _fixture.ChromeWebDriver.FindElement(By.Name("Passwd"));
+            webDriverWait.Until(d => d.Url.Contains("https://accounts.google.com/v3/signin/challenge/pwd"));
+            IWebElement passwordBox = chromeDriver.FindElement(By.Name("Passwd"));
+            webDriverWait.Until(d => passwordBox.Displayed);
             passwordBox.SendKeys(Configuration["password"]);
             passwordBox.SendKeys(Keys.Enter);
 
-            Thread.Sleep(2000);
-            Assert.Equal(("SenseFlow"), _fixture.ChromeWebDriver.Title);
+            webDriverWait.Until(d => d.Url.Contains("https://app.senseflow.ai/time"));
+            Assert.Equal(("SenseFlow"), chromeDriver.Title);
+        }
+
+        [Fact]
+        public void Dispose()
+        {
+            chromeDriver.Quit();
         }
     }
 }
